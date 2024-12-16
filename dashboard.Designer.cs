f1aaa4144a4c6d3df0cb8e56c2446ceff1ff854c
Sohaib Ikram <2022CS621@student.uet.edu.pk>
2024-12-16 20:31:16 +0500

    Login Page Done

--- dashboard.Designer.cs
+++ dashboard.Designer.cs
@@ -28,31 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dashboardControl1 = new DashboardControl();
             bookUserControl1 = new BookUserControl();
             SuspendLayout();
+            // 
+            // dashboardControl1
+            // 
+            dashboardControl1.BackColor = Color.Azure;
+            dashboardControl1.Location = new Point(326, -2);
+            dashboardControl1.Name = "dashboardControl1";
+            dashboardControl1.Size = new Size(1154, 852);
+            dashboardControl1.TabIndex = 1;
             // 
             // bookUserControl1
             // 
             bookUserControl1.BackColor = Color.Indigo;
-            bookUserControl1.Location = new Point(-1, 0);
+            bookUserControl1.Location = new Point(-1, -2);
             bookUserControl1.Margin = new Padding(4, 5, 4, 5);
             bookUserControl1.Name = "bookUserControl1";
-            bookUserControl1.Size = new Size(355, 1406);
-            bookUserControl1.TabIndex = 0;
+            bookUserControl1.Size = new Size(397, 1406);
+            bookUserControl1.TabIndex = 2;
             // 
             // dashboard
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1306, 789);
+            ClientSize = new Size(1425, 844);
             Controls.Add(bookUserControl1);
+            Controls.Add(dashboardControl1);
             Name = "dashboard";
             Text = "dashboard";
             ResumeLayout(false);
         }
 
         #endregion
-
+        private DashboardControl dashboardControl1;
         private BookUserControl bookUserControl1;
     }
 }
