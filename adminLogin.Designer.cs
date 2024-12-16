eb7574a93c1b5c5ed3002067ed8b0e635ee26410
Sohaib Ikram <2022CS621@student.uet.edu.pk>
2024-12-16 20:44:00 +0500

    AdminLogin Done

---
+++ adminLogin.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WinFormsApp1
+{
+    partial class adminLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(adminLogin));
+            textBox1 = new TextBox();
+            label7 = new Label();
+            pictureBox2 = new PictureBox();
+            label6 = new Label();
+            label1 = new Label();
+            label9 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(275, 180);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(198, 32);
+            textBox1.TabIndex = 15;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(151, 184);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(103, 23);
+            label7.TabIndex = 14;
+            label7.Text = "Password";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(249, 82);
+            pictureBox2.Margin = new Padding(4, 5, 4, 5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(155, 78);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 13;
+            pictureBox2.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Navy;
+            label6.Location = new Point(263, 9);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(126, 30);
+            label6.TabIndex = 12;
+            label6.Text = "TACBOOK";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(259, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(135, 23);
+            label1.TabIndex = 16;
+            label1.Text = "Admin Panel";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Century Gothic", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(272, 298);
+            label9.Name = "label9";
+            label9.Size = new Size(83, 23);
+            label9.TabIndex = 18;
+            label9.Text = "Cancel";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Cooper Black", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Navy;
+            button1.Location = new Point(249, 234);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 52);
+            button1.TabIndex = 17;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // adminLogin
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Azure;
+            ClientSize = new Size(630, 350);
+            Controls.Add(label9);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(pictureBox2);
+            Controls.Add(label6);
+            Name = "adminLogin";
+            Text = "adminLogin";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label7;
+        private PictureBox pictureBox2;
+        private Label label6;
+        private Label label1;
+        private Label label9;
+        private Button button1;
+    }
+}
