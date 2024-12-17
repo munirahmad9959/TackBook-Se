38571b8ac8c677bc8a458d22a592ccf2c940b46a
ZavAhmed <2022cs614@student.uet.edu.pk>
2024-12-17 21:03:05 +0500

    Wcf Services added

---
+++ WcfService1/DataList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class DataList
+    {
+        public static List<Order> mylist = new List<Order>();
+    }
+}
---
+++ WcfService1/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfService1
+{
+    public class Order
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string ClientName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
